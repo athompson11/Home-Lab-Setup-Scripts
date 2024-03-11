8be75d06aae3421a623f38fcf966bdb18684197e
Abigail <abbieadamsthompson@gmail.com>
2024-03-11 09:58:59 -0500

    Update proxy

--- modules/proxy/proxy.tf
+++ modules/proxy/proxy.tf
@@ -1,18 +1,12 @@
 # modules/proxy
-
-data "vsphere_virtual_machine" "ubuntu_template" {
-  name = "UbuntuTemplate" 
-}
-
 resource "vsphere_virtual_machine" "proxy_server" {
   name             = var.proxy_server_name
-  template_uuid    = data.vsphere_virtual_machine.ubuntu_template.id
-  resource_pool_id = data.vsphere_resource_pool.pool.id  # Assuming you get pool data
-  datastore_id     = data.vsphere_datastore.datastore.id  # Assuming you get datastore data
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
   num_cpus         = var.proxy_cpu_cores
   memory           = var.proxy_memory
 
   network_interface {
-    network_id = data.vsphere_network.network.id  # Assuming you get network data
+    network_id = data.vsphere_network.network.id
   }
 }
