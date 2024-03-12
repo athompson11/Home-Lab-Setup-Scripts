a1aa5c042cd19d2e2c8a6a8772ddc47eb61ec1c9
Abigail <abbieadamsthompson@gmail.com>
2024-03-12 15:23:29 -0500

    Add external dependency

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    macaddress = {
+      source = "ivoronin/macaddress"
+      version = "0.3.2"
+    }
+  }
+}
+
 provider "vsphere" {
   user                 = var.vsphere_username
   password             = var.vsphere_password
